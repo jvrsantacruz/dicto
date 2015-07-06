4ea771ee028bb5b37bcdbb246022c0817e731a36
jsl <jsl@taric.es>
2015-07-06 18:05:31 +0200

    Adds packaging info

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+REQUIREMENTS = open(os.path.join(here, 'requirements.txt')).readlines()
+
+setup(
+    name='dicto',
+    version='0.0.1',
+    description='Notification generator',
+    author='Javier Santacruz',
+    author_email='jsl@taric.es',
+    install_requires=REQUIREMENTS,
+    long_description=README,
+    py_modules=['dicto'],
+    packages=find_packages(),
+    classifiers=[
+        "Internal :: Do not upload"
+    ],
+    entry_points="""
+    [console_scripts]
+    dicto=dicto:cli
+    """
+)
